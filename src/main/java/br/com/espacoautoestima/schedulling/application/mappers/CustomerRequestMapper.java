a25525de84b045cab0c1c744a569a2f82e2ea3a5
David Castanheira de Souza <davidcastanheira.s@outlook.com>
2026-06-08 21:15:33 -0300

    chore: implementando mappers de request e response para a entidade 'Customer'

--- src/main/java/br/com/espacoautoestima/schedulling/application/mappers/CustomerRequestMapper.java
+++ src/main/java/br/com/espacoautoestima/schedulling/application/mappers/CustomerRequestMapper.java
@@ -2,16 +2,30 @@ package br.com.espacoautoestima.schedulling.application.mappers;
 
 import br.com.espacoautoestima.schedulling.application.adapters.dto.CustomerDTORequest;
 import br.com.espacoautoestima.schedulling.application.model.entities.CustomerEntity;
+import br.com.espacoautoestima.schedulling.domain.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface CustomerMapper {
+public interface CustomerRequestMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "name", source = "customerDTORequest.name")
     @Mapping(target = "email", source = "customerDTORequest.email")
     @Mapping(target = "phoneNumber", source = "customerDTORequest.phoneNumber")
     @Mapping(target = "cpf", source = "customerDTORequest.cpf")
-
     public abstract CustomerEntity toEntity(CustomerDTORequest customerDTORequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "customerDTORequest.name")
+    @Mapping(target = "email", source = "customerDTORequest.email")
+    @Mapping(target = "phoneNumber", source = "customerDTORequest.phoneNumber")
+    @Mapping(target = "cpf", source = "customerDTORequest.cpf")
+    public abstract Customer toDomain(CustomerDTORequest customerDTORequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "phoneNumber", source = "phoneNumber")
+    @Mapping(target = "cpf", source = "cpf")
+    public abstract CustomerDTORequest toDtoRequest(Customer customer);
 }
--- src/main/java/br/com/espacoautoestima/schedulling/application/mappers/CustomerResponseMapper.java
+++ src/main/java/br/com/espacoautoestima/schedulling/application/mappers/CustomerResponseMapper.java
@@ -1,4 +1,25 @@
 package br.com.espacoautoestima.schedulling.application.mappers;
 
+import br.com.espacoautoestima.schedulling.application.adapters.dto.CustomerDTOResponse;
+import br.com.espacoautoestima.schedulling.application.model.entities.CustomerEntity;
+import br.com.espacoautoestima.schedulling.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+// Check if is necessary includes 'uses' for other mappers
+@Mapper(componentModel = "spring")
 public interface CustomerResponseMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "customerEntity.name")
+    @Mapping(target = "email", source = "customerEntity.email")
+    @Mapping(target = "phoneNumber", source = "customerEntity.phoneNumber")
+    @Mapping(target = "cpf", source = "customerEntity.cpf")
+    public abstract CustomerDTOResponse toDtoResponse(CustomerEntity customerEntity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "customerEntity.name")
+    @Mapping(target = "email", source = "customerEntity.email")
+    @Mapping(target = "phoneNumber", source = "customerEntity.phoneNumber")
+    @Mapping(target = "cpf", source = "customerEntity.cpf")
+    public abstract Customer toDomain(CustomerEntity customerEntity);
 }
